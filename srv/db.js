bef92a318fa07f90ae113df35598c70d75bda529
avikinkitsolutions <avik.saha0104@hotmail.com>
2023-07-28 01:15:04 +0530

    Mongo Atlas Connected

--- srv/db.js
+++ srv/db.js
@@ -1,7 +1,8 @@
 const { default: mongoose } = require("mongoose");
 
 const connectDataBase = async () => {
-  const S_URL = "mongodb://127.0.0.1:27017/EmpDB";
+  // const S_URL = "mongodb://127.0.0.1:27017/EmpDB";
+  const S_URL = "mongodb+srv://aviksaha0104:JlxnPOr6CoTXBwhp@capmui.1tmshjf.mongodb.net/EmpDB";
   try {
     await mongoose.connect(S_URL);
     console.log("Database connected successfully");
